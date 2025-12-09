76062336f15e05dafad6d546d3d4ef6530399e19
Hasan TEKGÜL <htekgulds@gmail.com>
2025-12-09 22:43:19 +0300

    kullanılmayan dosyalar

--- server/db.js
+++
@@ -1,8 +0,0 @@
-import pg from "pg";
-import { DB } from "./config.js";
-
-const pool = new pg.Pool({
-  connectionString: DB.connectionString,
-});
-
-export const query = (text, params) => pool.query(text, params);
